562083a705f8d6aa3c75c926807e2949299bec81
Perceu Bertoletti <perceubertoletti@gmail.com>
2025-10-01 11:13:03 -0300

    ajustado para não quebrar a linha

--- chronos/settings.py
+++ chronos/settings.py
@@ -17,7 +17,7 @@ from dj_database_url import parse as db_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-VERSION='0.0.3'
+VERSION='0.0.4'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
