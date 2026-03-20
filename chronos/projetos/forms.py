fb9de742bba0e1d51649a82e896460fad8483bb3
Perceu Bertoletti <perceubertoletti@gmail.com>
2026-03-20 09:57:49 -0300

    ajustado para mostrar dados do cliente tambem dentro da area administrativa

--- chronos/projetos/forms.py
+++ chronos/projetos/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from chronos.empresas.baseForm import EmpresaModelForm
 from chronos.projetos.models import Projeto
 
@@ -6,10 +7,52 @@ from chronos.projetos.models import Projeto
 class ProjetoForm(EmpresaModelForm):
     class Meta:
         model = Projeto
-        fields = ["nome", "cliente", "valor", "pagamento", "status", "senha_acesso", "pago"]
+        fields = [
+            "nome",
+            "cliente",
+            "valor",
+            "pagamento",
+            "status",
+            "senha_acesso",
+            "pago",
+            "progresso",
+        ]
         widgets = {
-            'valor': forms.TextInput(attrs={
-                'class': "maskmoney",
-            }),
+            "valor": forms.TextInput(
+                attrs={
+                    "class": "maskmoney",
+                }
+            ),
         }
-    
+
+    # Campos públicos para o arquiteto enviar informações ao cliente
+    mensagem_publica = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "rows": 3,
+                "placeholder": "Digite uma mensagem pública para o cliente...",
+            }
+        ),
+        label="Mensagem Pública",
+    )
+
+    link_publico = forms.URLField(
+        required=False,
+        widget=forms.URLInput(
+            attrs={"class": "form-control", "placeholder": "https://..."}
+        ),
+        label="Link Público",
+    )
+
+    arquivo_publico = forms.FileField(
+        required=False,
+        widget=forms.ClearableFileInput(
+            attrs={
+                "class": "form-control",
+                "accept": ".pdf,.doc,.docx,.xls,.xlsx,.png,.jpg,.jpeg,.zip,.rar",
+            }
+        ),
+        label="Arquivo Público",
+    )
--- chronos/projetos/views.py
+++ chronos/projetos/views.py
@@ -1,17 +1,23 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from loguru import logger
 from django import forms
-from django.urls import reverse_lazy, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse, reverse_lazy
 from django.views.generic import (
-    ListView,
     CreateView,
-    UpdateView,
     DeleteView,
     DetailView,
+    ListView,
+    UpdateView,
 )
-from chronos.projetos.models import Projeto
+from loguru import logger
+
 from chronos.projetos.filters import ProjetoFilter
 from chronos.projetos.forms import ProjetoForm
+from chronos.projetos.models import (
+    Projeto,
+    ProjetoArquivo,
+    ProjetoComentario,
+    ProjetoLink,
+)
 
 
 class ProjetosListView(LoginRequiredMixin, ListView):
@@ -22,10 +28,10 @@ class ProjetosListView(LoginRequiredMixin, ListView):
         queryset = super().get_queryset()
         self.filterset = ProjetoFilter(self.request.GET, queryset=queryset)
         return self.filterset.qs
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['filter'] = self.filterset
+        context["filter"] = self.filterset
         return context
 
 
@@ -46,9 +52,46 @@ class ProjetosCreateView(LoginRequiredMixin, CreateView):
     def get_form(self):
         form = super().get_form()
         if not self.request.user.has_perm("projetos.can_view_payment_info"):
-            form.fields['valor'].widget = forms.widgets.HiddenInput()
-            form.fields['pago'].widget = forms.widgets.HiddenInput()
+            form.fields["valor"].widget = forms.widgets.HiddenInput()
+            form.fields["pago"].widget = forms.widgets.HiddenInput()
         return form
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        projeto = self.object
+        self._salvar_campos_publicos(projeto, form)
+
+        return response
+
+    def _salvar_campos_publicos(self, projeto, form):
+        """Salva os campos públicos (mensagem, link, arquivo) enviados pelo arquiteto"""
+
+        # Salvar mensagem pública
+        mensagem_publica = form.cleaned_data.get("mensagem_publica")
+        if mensagem_publica:
+            ProjetoComentario.objects.create(
+                projeto=projeto,
+                mensagem=mensagem_publica,
+                autor_cliente=False,  # Enviado pelo arquiteto
+            )
+
+        # Salvar link público
+        link_publico = form.cleaned_data.get("link_publico")
+        if link_publico:
+            ProjetoLink.objects.create(
+                projeto=projeto,
+                url=link_publico,
+                enviado_por_cliente=False,  # Enviado pelo arquiteto
+            )
+
+        # Salvar arquivo público
+        arquivo_publico = form.cleaned_data.get("arquivo_publico")
+        if arquivo_publico:
+            ProjetoArquivo.objects.create(
+                projeto=projeto,
+                arquivo=arquivo_publico,
+                enviado_por_cliente=False,  # Enviado pelo arquiteto
+            )
 
 
 class ProjetosUpdateView(LoginRequiredMixin, UpdateView):
@@ -78,9 +121,48 @@ class ProjetosUpdateView(LoginRequiredMixin, UpdateView):
     def get_form(self):
         form = super().get_form()
         if not self.request.user.has_perm("projetos.can_view_payment_info"):
-            form.fields['valor'].widget = forms.widgets.HiddenInput()
-            form.fields['pago'].widget = forms.widgets.HiddenInput()
+            form.fields["valor"].widget = forms.widgets.HiddenInput()
+            form.fields["pago"].widget = forms.widgets.HiddenInput()
         return form
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # Processar campos públicos (enviados pelo arquiteto, não pelo cliente)
+        projeto = self.object
+        self._salvar_campos_publicos(projeto, form)
+
+        return response
+
+    def _salvar_campos_publicos(self, projeto, form):
+        """Salva os campos públicos (mensagem, link, arquivo) enviados pelo arquiteto"""
+
+        # Salvar mensagem pública
+        mensagem_publica = form.cleaned_data.get("mensagem_publica")
+        if mensagem_publica:
+            ProjetoComentario.objects.create(
+                projeto=projeto,
+                mensagem=mensagem_publica,
+                autor_cliente=False,  # Enviado pelo arquiteto
+            )
+
+        # Salvar link público
+        link_publico = form.cleaned_data.get("link_publico")
+        if link_publico:
+            ProjetoLink.objects.create(
+                projeto=projeto,
+                url=link_publico,
+                enviado_por_cliente=False,  # Enviado pelo arquiteto
+            )
+
+        # Salvar arquivo público
+        arquivo_publico = form.cleaned_data.get("arquivo_publico")
+        if arquivo_publico:
+            ProjetoArquivo.objects.create(
+                projeto=projeto,
+                arquivo=arquivo_publico,
+                enviado_por_cliente=False,  # Enviado pelo arquiteto
+            )
 
 
 class ProjetosDeleteView(LoginRequiredMixin, DeleteView):
@@ -90,3 +172,22 @@ class ProjetosDeleteView(LoginRequiredMixin, DeleteView):
 
 class ProjetosDetailView(LoginRequiredMixin, DetailView):
     model = Projeto
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        projeto = self.object
+
+        # Carrega comentários do projeto
+        context["comentarios"] = ProjetoComentario.objects.filter(
+            projeto=projeto
+        ).order_by("-criado_em")
+
+        # Carrega arquivos do projeto
+        context["arquivos"] = ProjetoArquivo.objects.filter(projeto=projeto)
+
+        # Carrega links do projeto
+        context["links"] = ProjetoLink.objects.filter(projeto=projeto).order_by(
+            "-criado_em"
+        )
+
+        return context
